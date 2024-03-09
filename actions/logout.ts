e29ac0329c3ecd1f61fa6c314483575e50bdea7d
Tommy <86784639+tommy88520@users.noreply.github.com>
2024-03-09 17:36:42 +0800

    feat: reset password and 2fa

--- actions/logout.ts
+++ actions/logout.ts
@@ -3,5 +3,6 @@
 import { signOut } from "@/auth";
 
 export const logout = async () => {
+  //會拆成這樣寫可能是要做一些打api之類的事情server端的事
   await signOut();
 };
--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,12 +1,15 @@
+"use client";
+
 import { auth } from "@/auth";
 import { LogoutButton } from "@/components/auth/logout-button";
 import { ExitIcon } from "@radix-ui/react-icons";
-const SettingPage = async () => {
-  const session = await auth();
-
+import { useCurrentUser } from "@/hooks/use-current-user";
+//signOut 用在client 或server端都可以
+const SettingPage = () => {
+  const user = useCurrentUser;
   return (
     <div>
-      {JSON.stringify(session)}
+      {JSON.stringify(user)}
       <LogoutButton>
         <ExitIcon className="h-4 w-4 mr-2" />
         Logout
